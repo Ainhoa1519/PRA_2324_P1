235e29ff35ce72c3a95be2d7f8097f4bc836333a
Ainhoa <alopgom3@upv.edu.es>
2025-11-13 09:55:07 +0100

    Update List.h

--- List.h
+++ List.h
@@ -4,14 +4,14 @@
 template <typename T>
 class List{
 	public:
-	virtual void insert (int pos,const T& e)=0;
-	virtual void append (T e)=0;
-	virtual void prepend (T e)=0;
-	virtual T remove(int pos)=0;
-	virtual T get (int pos)=0;
-	virtual int search(const T& e)const=0;
-	virtual bool empty()const=0;
-	virtual int size()const=0;
+	virtual void insert (int pos,const T& e)=0;	//insertar elemento e en pos
+	virtual void append (T e)=0;	//insertar e al final de la lista
+	virtual void prepend (T e)=0;	//insertar e al principio de la lista
+	virtual T remove(int pos)=0;	//elimina y devuelve el elmento que esté en pos
+	virtual T get (int pos)=0;		//devuelve el elemento de pos
+	virtual int search(const T& e)const=0;	//devuelve la 1º pos donde se encuentre e y si no lo encuentra devuelve -1
+	virtual bool empty()const=0;	//indica si la lista está vacía
+	virtual int size()const=0;		//devuelve el número de elementos de la lista (n)
 };
 
 #endif
