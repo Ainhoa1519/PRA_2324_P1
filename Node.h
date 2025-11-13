eba661572f03b982bab2baad19a62724f8b77e1d
Ainhoa <alopgom3@upv.edu.es>
2025-11-13 10:45:48 +0100

    Añadida implementación de la clase Node

--- Node.h
+++ Node.h
@@ -3,15 +3,15 @@
 template <typename T>
 class Node {
 public:
-    T data;                    // Elemento almacenado
+    T data;                    // Elemento almacenado tipo genérico
     Node<T>* next;             // Puntero al siguiente nodo
-
+//metodo constructor: next será nullptr en caso de que no se proporcione
     Node(T data, Node<T>* next = nullptr) : data(data), next(next) {
     
     }  
-
+//sobrecarga el operador << para imprimir una instancia de Node<T> por pantalla
     friend std::ostream& operator<<(std::ostream& out, const Node<T>& node) {
-        out << node.data;      // Imprime solo el dato
+        out << node.data;      // Imprime solo el dato/ atributo
         return out;
     }
 };
